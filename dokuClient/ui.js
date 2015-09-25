813d2b0360684dfe1fb52b7fa4e838c9d680d9e8
Tom Brewe <tom.brewe@fu-berlin.de>
2015-09-25 15:58:42 +0200

    new UI prototype, cleanup and prepare work for modular web components

--- dokuClient/ui.js
+++ dokuClient/ui.js
@@ -7,60 +7,86 @@ var localDB = new PouchDB('annotations')
 var remoteDB = new PouchDB('http://127.0.0.1:5984/annotations')
 
 var annotations
+var annotationElements = new Map()
 
 // DOM elements
 var imageContainer
 
-function rebuildUI() {
-	imageContainer = document.querySelector('.imageContainer')
-	var overlays = imageContainer.querySelectorAll('.annotationOverlay')
-	// in chrome45/electron 0.32: Array.from(imageContainer.querySelectorAll('.annotationOverlay'))
 
-	// first clean old overlays
-	for (var i = 0; i < overlays.length; i++) {
-		imageContainer.removeChild(overlays[i])
+function removeAnnotationElements(id) {
+	// get DOM objects belonging to id
+	let [annotationPoint, annotationBox] = annotationElements.get(id)
+	annotationPoint.parentNode.removeChild(annotationPoint)
+	annotationBox.parentNode.removeChild(annotationBox)
+}
+
+
+function addAnnotationElements({doc: {name, description, position, _id, _attachments}})
+	// add both, annotation box and annotation point to DOM
+	// only handle creation of DOM element, actual DB updates
+	// are done independently
+	
+	imageContainer = document.querySelector('.object-view')
+	let annotationPoint = document.createElement('div')
+	let annotationBox = document.createElement('annotationBox')
+	annotationPoint.classList.add('annotationPoint')
+
+	if (position[0] === undefined) {
+		throw Error('position[0] === undefined', doc)
 	}
 	
-	// ES6 object destructuring comes really handy here!
-	// see: https://leanpub.com/understandinges6/read#leanpub-auto-object-destructuring
-	// console.log(annotations)
-	for (let {doc: {name, description, position, _id, _attachments}} of annotations) {
-		let overlay = document.createElement('div')
-		overlay.classList.add('annotationOverlay')
-		if(position[0] === undefined) console.log(doc)
-		overlay.style.position = 'absolute'
-		overlay.style.left = position[0] + 'px'
-		overlay.style.top = position[1] + 'px'
-		overlay.innerHTML = description
-		overlay.contentEditable = true
-		overlay.id = _id
-overlay.addEventListener('blur', function({target: {id, innerHTML}}) {
+	annotationPoint.style.position = 'absolute'
+	annotationPoint.style.left = position[0] + 'px'
+	annotationPoint.style.top = position[1] + 'px'
+	annotationPoint.innerHTML = description
+	annotationPoint.contentEditable = true
+	annotationPoint.id = _id
 	
-	// update document in localDB
-	localDB.get(id).then(doc => {
-		console.log('log doc')
-		console.log(doc)
-		return localDB.put({
-			_id: id,
-			_rev: doc._rev,
-			description: innerHTML,
-			position: doc.position,
-			name: doc.name,
-			_annotations: doc._annotations
+	annotationPoint.addEventListener('blur', function({target: {id, innerHTML}}) {
+
+		// TODO: move this event listener to the web components code later
+		// update document in localDB
+		localDB.get(id).then(doc => {
+
+			return localDB.put({
+				_id: id,
+				_rev: doc._rev,
+				description: innerHTML,
+				position: doc.position,
+				name: doc.name,
+				_annotations: doc._annotations
+			})
+
+		}).then(response => {
+			console.log(response)
+		}).catch(err => {
+			console.log(err)
 		})
-	}).then(response => {
-		console.log(response)
-		// handle response
-	}).catch(err => {
-		console.log(err)
+
+
 	})
+	
+	imageContainer.appendChild(annotationPoint)
+	imageContainer.appendChild(annotationBox)
+	annotationElements.set(_id, [annotationPoint, annotationBox])
+
+}
 
 
-})
+function rebuildAnnotations() {
+	// this function removes all created representations for annotations
+	// and re-creates and appends them to the view
 
-		imageContainer.appendChild(overlay)
+	// first clean old annotation elements
+	for (let id of annotationElements.keys()) {
+		removeAnnotationElements(id)
 	}
-
+	
+	// then add new annotation elements
+	for (let annotation of annotations) {
+		addAnnotationElements(annotation)
+	}
+	
 }
 
 function fetchAnnotations() {
@@ -86,34 +112,51 @@ window.addEventListener('online', alertOnlineStatus)
 window.addEventListener('offline', alertOnlineStatus)
 
 var sync = PouchDB.sync(localDB, remoteDB, {
-  live: true,
-  retry: true
-}).on('change', function (info) {
+	live: true,
+	retry: true
+}).on('change', function(info) {
 
 	// only rebuild UI if there are changes after a pull from remoteDB
-	if(info.direction === 'pull' && info.change.docs.length !== 0) {
-		fetchAnnotations().then(() => {rebuildUI()})
+	if (info.direction === 'pull' && info.change.docs.length !== 0) {
+		// console.log(info)
+		// FIXME: instead of completely rebuilding
+		// just delete/modifiy/add the ones that changed
+
+		// go through all changes
+		// trigger delete actions and edit/actions
+		// for (let annotation of info.change.docs) {
+		// 	console.log(annotation)
+		// 		if(annotation._deleted === true){
+		// 				let
+		// 		} else {
+		//
+		// 		}
+		// }
+
+		fetchAnnotations().then(() => {
+			rebuildAnnotations()
+		})
 	}
 
 }).on('paused', () => {
 	console.log('sync pause')
 
-  // replication paused (e.g. user went offline)
+	// replication paused (e.g. user went offline)
 }).on('active', () => {
 	console.log('sync active')
 
-  // replicate resumed (e.g. user went back online)
+	// replicate resumed (e.g. user went back online)
 }).on('denied', info => {
 	console.log('sync denied')
 
-  // a document failed to replicate, e.g. due to permissions
+	// a document failed to replicate, e.g. due to permissions
 }).on('complete', info => {
 	console.log('sync complete')
-  // handle complete
+		// handle complete
 }).on('error', err => {
 	console.log('sync error')
 
-  // handle error
+	// handle error
 })
 
 function init() {
@@ -130,7 +173,7 @@ function init() {
 		console.error(err)
 	})
 
-	fetchAnnotations().then(() => rebuildUI())
+	fetchAnnotations().then(() => rebuildAnnotations())
 
 }
 
@@ -155,32 +198,32 @@ init()
 // 	let img = imgContainer.querySelector('img')
 // 	img.src = 'data:image/jpg;base64,' + annotation.image
 //
-// 	let overlay = document.createElement('div')
-// 	overlay.style.position = 'absolute'
-// 	overlay.style.left = annotation.position[0] + 'px'
-// 	overlay.style.top = annotation.position[1] + 'px'
-// 	overlay.innerHTML = annotation.description
-// 	overlay.contentEditable = true
+// 	let annotationPoint = document.createElement('div')
+// 	annotationPoint.style.position = 'absolute'
+// 	annotationPoint.style.left = annotation.position[0] + 'px'
+// 	annotationPoint.style.top = annotation.position[1] + 'px'
+// 	annotationPoint.innerHTML = annotation.description
+// 	annotationPoint.contentEditable = true
 //
 // 	let audio = document.createElement('audio')
 // 		// audio.src = 'test.wav'
 //
-// 	overlay.addEventListener('mouseover', function(e) {
+// 	annotationPoint.addEventListener('mouseover', function(e) {
 // 		audio.play()
 // 	})
-// 	overlay.addEventListener('mouseout', function(e) {
+// 	annotationPoint.addEventListener('mouseout', function(e) {
 // 		audio.pause()
 // 	})
 //
-// 	overlay.appendChild(audio)
+// 	annotationPoint.appendChild(audio)
 //
-// 	overlay.addEventListener('input', function(e) {
+// 	annotationPoint.addEventListener('input', function(e) {
 // 		console.log('annotation changed')
 // 		console.log(e)
 // 			// probably use a MVC lib here (angular?, react)
 // 			// ipc.send('annotation_changed', {id:1, newAnnotation: })
 // 	})
-// 	imgContainer.appendChild(overlay)
+// 	imgContainer.appendChild(annotationPoint)
 //
 // })
 
