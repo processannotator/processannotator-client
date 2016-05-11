d52395597f6c761554f0c59209d9072b4a488a00
tom <tombrwe@gmail.com>
2016-05-11 20:51:40 +0200

    added systemd service

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var config = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf8'));
 
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({ port: 7000 });
